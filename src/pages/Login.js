f03f92966d6d9766e27e25fdd0e40cf1c9e7b7c0
Jonathan Ferreira <jonathanferreira23@gmail.com>
2019-08-16 22:07:03 +0100

    Pre-Animation commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,88 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function Login() {
-  return <View style={styles.container} />;
+import api from '../services/api';
+import logo from '../assets/images/logo.png';
+
+export default function Login({navigation}) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', {user});
+      }
+    });
+  }, [navigation]);
+
+  async function handleLogin() {
+    const response = await api.post('/devs', {username: user});
+    const {_id} = response.data;
+    await AsyncStorage.setItem('user', _id);
+    navigation.navigate('Main', {user: _id});
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}>
+      <Image source={logo} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Degite seu usuário do Github"
+        style={styles.input}
+        value={user}
+        onChangeText={setUser}
+      />
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.text}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 16,
+  },
 });
